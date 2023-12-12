497c193aad7d0634d26de5289f60478423daae1e
Fayiz-hdsb <1alifay@hdsb.ca>
2023-12-12 11:10:11 -0500

    Added bot chasing code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Finchbot.BirdBrain import Finch
 from time import sleep
+from enum import Enum
 
 DELAY_VALUE:float = 0.1 #.1 of a second = 100 milliseconds
 
@@ -18,31 +19,39 @@ def adjustIfOnBoundary():
 
 moving:bool = False
 
+class BotStateEnum(Enum):
+    detecting = 'detecting'
+    chasing = 'chasing'
+    goingBehind = 'goingBehind'
+
+botStateEnum:BotStateEnum = BotStateEnum.detecting
+
 while True:
     dist = finch.getDistance()
 
     adjustIfOnBoundary()
 
-    if(not moving):
+    if(botStateEnum == BotStateEnum.detecting):
         rotate()
-
-    if(dist<50):
+        if(dist<50):
         #bot is close
-        if(dist>=(prevDist)):
-            #the other bot is moving is running away or is still, so slam it out of the ring.
-            print('Bot running away from me')
-            # finch.setMotors(100, 100)
-            # finch.setMotors(5, 5)
-            finch.setMotors(0, 0) 
-            moving = True
+            if(dist>=(prevDist)):
+                #the other bot is moving is running away or is still, so slam it out of the ring.
+                print('Bot running away from me')
+                botStateEnum = BotStateEnum.chasing
+            else:
+            #the other bot is coming towards us.
+                print('Bot coming towards me')
+                botStateEnum = BotStateEnum.goingBehind
         else:
-        #the other bot is coming towards us.
-            print('Bot coming towards me')
-            moving = True
-            goBehind()
-    else:
-        print('No bot in my range')
-        moving=False
+            print('No bot in my range')
+            moving=False
+    elif(botStateEnum == BotStateEnum.chasing):
+        # finch.setMotors(100, 100)
+        finch.setMotors(5, 5)
+        # finch.setMotors(0, 0)
+    elif(botStateEnum == BotStateEnum.goingBehind):
+        goBehind()
 
     prevDist = dist
 
